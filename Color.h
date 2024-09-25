b7e23315433b4f64eced9ab73ee3e0ddfbc70b1b
vignesh-prakash <vignesh24p@gmail.com>
2024-09-25 21:11:44 +0530

    Create Color.h

---
+++ Color.h
@@ -0,0 +1,17 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void PrintColorReferenceManual();
+
+#endif
